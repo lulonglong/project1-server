291a5c3b32829bd1a2585709896fd7cdf0b21b58
lulonglong <luguo3000@126.com>
2013-07-01 12:12:46 +0800

    添加请求过滤

---
+++ src/locationshare/common/filter/RequestFilter.java
@@ -0,0 +1,56 @@
+package locationshare.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import locationshare.common.util.StringUtil;
+
+
+/**
+ * Servlet Filter implementation class RequestFilter
+ */
+public class RequestFilter implements Filter {
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		if (request instanceof HttpServletRequest) {
+
+			HttpServletRequest req = (HttpServletRequest) request;
+			String userAgentString=req.getHeader("user-agent");
+			if (req.getMethod() == "GET"||!StringUtil.isMobileClient(userAgentString)) {
+				HttpServletResponse res = (HttpServletResponse) response;
+				res.sendError(HttpServletResponse.SC_FORBIDDEN);
+				return;
+			}
+			
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
