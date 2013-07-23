76ce54eb86d7b35a6656004948cd1ec1f263b84b
lulonglong <luguo3000@126.com>
2013-07-23 18:16:28 +0800

    edit upload headportrait

--- src/locationshare/servlet/UpdateHeadPortraitServlet.java
+++ src/locationshare/servlet/UpdateHeadPortraitServlet.java
@@ -1,9 +1,16 @@
 package locationshare.servlet;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 
 import locationshare.action.ProfileAction;
 import locationshare.base.servlet.BaseServlet;
@@ -17,9 +24,32 @@ public class UpdateHeadPortraitServlet extends BaseServlet {
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse res)
 			throws IOException {
-		String typeString = req.getParameter("type");
-		String usernameString = req.getParameter("username");
-		String passwordString = req.getParameter("password");
+
+		String userid = req.getParameter("userid");
+		ServletInputStream inputStream = null;
+		BufferedOutputStream outputStream = null;
+
+		try {
+			inputStream = req.getInputStream();
+			outputStream = new BufferedOutputStream(new FileOutputStream(
+					"D:/1.png"));
+			byte[] buffer = new byte[10000];
+			int count = 0;
+
+			while ((count = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, count);
+			}
+			outputStream.flush();
+
+		} catch (Exception e) {
+			logger.error("get image failed");
+		} finally {
+			if (inputStream != null)
+				inputStream.close();
+			if (outputStream != null)
+				outputStream.close();
+		}
+
 		return "profile";
 	}
 
